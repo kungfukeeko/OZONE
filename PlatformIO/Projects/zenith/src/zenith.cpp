e07383620199183274fbd84391e8e258b2ea38fb
kungfukeeko <kungfukeeko@gmail.com>
2026-06-01 14:32:40 +0200

    Setting up branch zenith

---
+++ PlatformIO/Projects/zenith/src/zenith.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include "ADS131M04.h"
+#include <ESP32Servo.h>
+#include <TimeLib.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
